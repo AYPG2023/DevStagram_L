b4ccd92cea30fc19633918a9b9d6088c5fecd096
AYPG2023 <andersonperdomo525@gmail.com>
2024-12-04 16:01:53 -0600

    Avances del formulario de campos requeridos en los campos de creacion de cuenta

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,9 +17,15 @@ class RegisterController extends Controller
 
      //Validación.
      $this->validate( $request,[
-          'name' => 'required|min:5',      // Esto funciona para poder validar cuales son los campos obligatorios en el formulario 
-                                     // de crear cuenta tambien dentro de esa validacion se pueden agregar otras validaciones y puede mostrar mensajes poder defecto. 
-                                         
+          'name' => 'required|max:30',      // Esto funciona para poder validar cuales son los campos obligatorios en el formulario tambien se puede poner como arreglo [n , m] 
+          'username' => 'required|unique:users|min:3|max:20',  // de crear cuenta tambien dentro de esa validacion se pueden agregar otras validaciones y puede mostrar mensajes poder defecto. 
+          'email' =>'required|unique:users|email|max:60',      
+          'password' => 'required',                          
      ]); 
 }   // Funciona para crear un metodo tipo post y hacer consultas
 }
+
+
+
+//'required|unique:users' funciona para validar en la base de datos en la tabla de usuarios que ningun otro usuario tenga ese usuario.
+//required|unique:users|email|max:60 Esto valida que sea un email y que tenga una capacidad de 60 caracteres. 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,26 +10,40 @@
         <img src="{{ asset('img/registrar.jpg')}}" alt="Imagen de registro de usuario">
     </div>
     <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl"> <!--Crea una sombra shadow -->
-        <form action="{{route('register')}}" method="POST">  <!--Esto funciona para que este formulario tenga una accion y se pueda cargar la infor desde las peticiones tipo post-->
+        <form action="{{route('register')}}" method="POST" novalidate>  <!--Esto funciona para que este formulario tenga una accion y se pueda cargar la infor desde las peticiones tipo post-->
            @csrf <!--Esto hace que se comniqyue perfectamente con las peticiones -->
             <div class="mb-5">
                 <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre</label>
-                <input id="name" name="name" type="text" placeholder="Tu Nombre" class="border p-3 w-full rounded-lg">
+                <input id="name" name="name" type="text" placeholder="Tu Nombre" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" 
+                value= "{{old('name')}}" > <!--value= old funciona para que al momento que se envie el formulario y se recargue la pagina por un error se muestren los datos que se llenaron 
+                antes de enviar el formulario dentro de old(va el nombre de la variable) -->
                 @error('name') <!--Esto funciona para poder crear un error al momento que se este llenado el formulario y depende la validacion muestre un error -->
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                 @enderror
             </div>
             <div class="mb-5">
                 <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Username</label>
-                <input id="username" name="username" type="text" placeholder="Nombre de usuario" class="border p-3 w-full rounded-lg">
+                <input id="username" name="username" type="text" placeholder="Nombre de usuario" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" 
+                value= "{{old('name')}}" >
+                @error('username') <!--Esto funciona para poder crear un error al momento que se este llenado el formulario y depende la validacion muestre un error -->
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-5">
                 <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
-                <input id="email" name="email" type="email" placeholder="Ingresa tu email" class="border p-3 w-full rounded-lg">
+                <input id="email" name="email" type="email" placeholder="Ingresa tu email" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" 
+                value= "{{old('name')}}">
+                @error('email') <!--Esto funciona para poder crear un error al momento que se este llenado el formulario y depende la validacion muestre un error -->
+                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-5">
                 <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
-                <input id="password" name="password" type="password" placeholder="Ingresa tu password" class="border p-3 w-full rounded-lg">
+                <input id="password" name="password" type="password" placeholder="Ingresa tu password" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" 
+                value= "{{old('name')}}" >
+                @error('password') <!--Esto funciona para poder crear un error al momento que se este llenado el formulario y depende la validacion muestre un error -->
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-5">
                 <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">Repetir Password</label>
